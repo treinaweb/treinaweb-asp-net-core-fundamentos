bcb5b892ffe644e8f383139cfffa237a2872d134
Cleyson Lima <cleysonph@gmail.com>
2022-05-07 02:53:03 -0300

    05:04 - criando nosso primeiro controller e nossa primeira action

---
+++ Controllers/TestController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TWTodoList.Controllers;
+
+public class TestController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
